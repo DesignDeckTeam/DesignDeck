6fb99400312ee1e481d48e37cb7157ae78909e23
Ray Gao <atlantis.ray@gmail.com>
2016-11-11 18:25:58 +0800

    submit samples

--- app/controllers/designer/orders_controller.rb
+++ app/controllers/designer/orders_controller.rb
@@ -13,13 +13,46 @@ class Designer::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @version = @order.versions.update(order_params)
-      redirect_to designer_order_path, notice: "Update Success"
+
+    if order_params[:image1_from_designer].present? 
+      if @order.versions.first.blank?
+        version1 = @order.versions.build
+      else
+        version1 = @order.versions.first
+      end
+      version1.image_from_designer = order_params[:image1_from_designer]
+      version1.save
+    end
+
+    if order_params[:image2_from_designer].present? 
+      if @order.versions.second.blank?
+        version2 = @order.versions.build
+      else
+        version2 = @order.versions.second
+      end
+      version2.image_from_designer = order_params[:image2_from_designer]
+      version2.save
+    end
+
+    if order_params[:image3_from_designer].present? 
+      if @order.versions.third.blank?
+        version3 = @order.versions.build
+      else
+        version3 = @order.versions.third
+      end
+      version3.image_from_designer = order_params[:image3_from_designer]
+      version3.save
+    end    
+
+    redirect_to designer_order_path, notice: "Update Success"
+
   end
 
 private
 
   def order_params
-    params.require(:order).permit(versions_attributes:[:image_from_designer], versions_attributes:[:image_from_customer])
+    # params.require(:order).permit(versions_attributes:[:image_from_designer], versions_attributes:[:image_from_customer])
+    params.require(:order).permit(:image1_from_designer, :image2_from_designer, :image3_from_designer)
+
   end
 end
