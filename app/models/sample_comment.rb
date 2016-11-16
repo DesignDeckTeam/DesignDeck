71c371415a15d7af7d22cb7eb1d4f73c34f6f848
zengzheng <zengzheng2009@gmail.com>
2016-11-16 09:44:03 +0800

    no change

--- app/models/sample_comment.rb
+++ app/models/sample_comment.rb
@@ -10,7 +10,6 @@ end
 #  id         :integer          not null, primary key
 #  sample_id  :integer
 #  user_id    :integer
-#  content    :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
