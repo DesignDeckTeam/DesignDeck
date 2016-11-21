90e492687f6a8f753ab14681100b03de758bf2e7
mapuying <mapuying@gmail.com>
2016-11-21 09:01:52 +0800

    devise choice designer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   def designer_required
@@ -16,5 +17,12 @@ class ApplicationController < ActionController::Base
   		end
   	end
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+    end
 
 end
