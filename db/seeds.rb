03b0dd8cbf89bde271a050e80a77838997c2493f
Ray Gao <atlantis.ray@gmail.com>
2016-11-22 11:00:23 +0800

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,6 @@ create_account = User.create([email: 'user1@designerdeck.com', password: '111111
 create_account = User.create([email: 'user2@designerdeck.com', password: '111111', password_confirmation: '111111', is_designer: 'false'])
 
 
-create_order = Order.create([title: "咖啡公司咖啡杯设计", description: "需要设计一款咖啡杯，体现出简洁优雅的风格", user_id: 2, preference_type: "LOGO", aasm_state: "paid"])
-create_order = Order.create([title: "委托设计一系列统计图标", description: "需要有柱状图，饼图，和好看的色彩", user_id: 2, preference_type: "WEBDESIGN"])
-create_order = Order.create([title: "大学圣诞创意作品大奖赛主题演讲幻灯片设计", description: "体现圣诞的气氛，加入欢乐的元素", user_id: 2, preference_type: "PRESENTATION"])
+create_order = Order.create([title: "咖啡公司咖啡杯设计", description: "需要设计一款咖啡杯，体现出简洁优雅的风格", user_id: 3, preference_type: "LOGO", aasm_state: "paid"])
+create_order = Order.create([title: "委托设计一系列统计图标", description: "需要有柱状图，饼图，和好看的色彩", user_id: 3, preference_type: "WEBDESIGN"])
+create_order = Order.create([title: "大学圣诞创意作品大奖赛主题演讲幻灯片设计", description: "体现圣诞的气氛，加入欢乐的元素", user_id: 3, preference_type: "PRESENTATION"])
