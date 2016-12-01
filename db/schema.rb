7ede95d764c7c97b56b4d19f573e2bb1696cb635
Ray Gao <atlantis.ray@gmail.com>
2016-12-01 21:25:38 +0800

    downloaded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130152501) do
+ActiveRecord::Schema.define(version: 20161201104042) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.string  "unsubscriber_type"
